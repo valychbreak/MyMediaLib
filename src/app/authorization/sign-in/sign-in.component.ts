8dd3125491fa82205f4561eec62916c82b5b6465
valych <valychbreak@gmail.com>
2017-01-21 23:07:08 +0200

    Add sign in form

--- src/app/authorization/sign-in/sign-in.component.ts
+++ src/app/authorization/sign-in/sign-in.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 import {MovieDetailsModalComponent} from "../../movies/movie-details-modal/movie-details-modal.component";
+import {User} from "../../users/shared/user";
 
 @Component({
   selector: 'app-sign-in',
@@ -11,16 +12,23 @@ import {MovieDetailsModalComponent} from "../../movies/movie-details-modal/movie
 export class SignInComponent implements OnInit {
   title: string;
   model = 1;
+  user: User;
 
   constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
     this.title = "Sign in"
+
+    this.user = new User();
   }
 
   open() {
     const modalRef = this.modalService.open(MovieDetailsModalComponent);
     modalRef.componentInstance.name = 'World';
+  }
+
+  save(userModel: User, isValid: boolean) {
+    console.log(userModel, isValid);
   }
 
 }
