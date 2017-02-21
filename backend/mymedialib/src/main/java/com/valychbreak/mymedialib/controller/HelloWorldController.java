254ce793956857e5da48623c36487f6cb260379f
valych <valychbreak@gmail.com>
2017-02-21 18:42:47 +0200

    Add configured spring-mvc project

---
+++ backend/mymedialib/src/main/java/com/valychbreak/mymedialib/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.valychbreak.mymedialib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by valych on 2/21/17.
+ */
+@Controller
+public class HelloWorldController {
+    @GetMapping("/")
+    public String showIndexPage(Model model) {
+        model.addAttribute("name", "John Doe");
+
+        return "welcome";
+    }
+
+    @GetMapping("/hello")
+    public String showHelloPage(Model model) {
+        model.addAttribute("message", "Hello message");
+
+        return "hellopage";
+    }
+}
---
+++ backend/mymedialib/src/main/webapp/WEB-INF/jsp/hellopage.jsp
@@ -0,0 +1,13 @@
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello page</title>
+    <%--<link rel='stylesheet' href='<c:url value="/resources/css/style.css" />' type='text/css' media='all' />--%>
+</head>
+<body>
+<h2>Hello Page</h2>
+
+<p>Message is: ${message}</p>
+</body>
+</html>
