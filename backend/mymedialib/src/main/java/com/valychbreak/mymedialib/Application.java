b366d6a9fd5bf94515a734d2c7338e89a87b5da2
valych <valychbreak@gmail.com>
2017-02-25 16:28:52 +0200

    Code refactoring

--- backend/mymedialib/src/main/java/com/valychbreak/mymedialib/Application.java
+++ backend/mymedialib/src/main/java/com/valychbreak/mymedialib/Application.java
@@ -1,11 +1,12 @@
 package com.valychbreak.mymedialib;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
@@ -14,19 +15,22 @@ import java.util.Arrays;
  */
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { JacksonAutoConfiguration.class })
-//@ComponentScan(basePackages = "com.valychbreak.mymedialib")
 public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        //printAllBeans(ctx);
+    }
+
+    private static void printAllBeans(ApplicationContext ctx) {
+        log.info("Let's inspect the beans provided by Spring Boot:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
-            System.out.println(beanName);
+            log.info(beanName);
         }
     }
-
 }
