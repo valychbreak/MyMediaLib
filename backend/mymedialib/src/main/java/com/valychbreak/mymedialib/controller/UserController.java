d2ae914305d2cfe4d2e2c3f5b99e790f5f6fc245
valych <valychbreak@gmail.com>
2017-02-25 18:05:09 +0200

    Add mapping for adding a user

--- backend/mymedialib/src/main/java/com/valychbreak/mymedialib/controller/UserController.java
+++ backend/mymedialib/src/main/java/com/valychbreak/mymedialib/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +37,13 @@ public class UserController {
 
         userRepository.findAll();
         return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/user/add", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User createdUser = userRepository.save(user);
+
+        return new ResponseEntity<User>(createdUser, HttpStatus.OK);
     }
 }
