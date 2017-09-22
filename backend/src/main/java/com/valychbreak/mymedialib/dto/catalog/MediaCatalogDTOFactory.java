30108c46ce335cc0fb8c63036b860784016aee80
valych <valychbreak@gmail.com>
2017-09-22 21:31:02 +0300

    Added method create MediaCatalogDTO without media

--- backend/src/main/java/com/valychbreak/mymedialib/dto/catalog/MediaCatalogDTOFactory.java
+++ backend/src/main/java/com/valychbreak/mymedialib/dto/catalog/MediaCatalogDTOFactory.java
@@ -11,15 +11,27 @@ import java.util.List;
 
 
 public class MediaCatalogDTOFactory {
-    public MediaCatalogDTO create(UserMediaCatalog userMediaCatalog) throws IOException, OMDBException {
-        List<MediaFullDetailsImpl> mediaList = new ArrayList<>();
-        for (UserMedia userMedia : userMediaCatalog.getUserMediaList()) {
-            mediaList.add((MediaFullDetailsImpl) userMedia.getMedia().getDetails());
+    public MediaCatalogDTO createWithMedia(UserMediaCatalog userMediaCatalog) throws IOException, OMDBException {
+        return create(userMediaCatalog, true);
+    }
+
+    public MediaCatalogDTO createWithoutMedia(UserMediaCatalog userMediaCatalog) throws IOException, OMDBException {
+        return create(userMediaCatalog, false);
+    }
+
+    protected MediaCatalogDTO create(UserMediaCatalog userMediaCatalog, boolean includeMedia) throws IOException, OMDBException {
+        List<MediaFullDetailsImpl> mediaList = null;
+
+        if(includeMedia) {
+            mediaList = new ArrayList<>();
+            for (UserMedia userMedia : userMediaCatalog.getUserMediaList()) {
+                mediaList.add((MediaFullDetailsImpl) userMedia.getMedia().getDetails());
+            }
         }
 
         List<MediaCatalogDTO> subCatalogs = new ArrayList<>();
         for (UserMediaCatalog subCatalog : userMediaCatalog.getSubUserMediaCatalogs()) {
-            MediaCatalogDTO mediaCatalogDTO = create(subCatalog);
+            MediaCatalogDTO mediaCatalogDTO = create(subCatalog, includeMedia);
             subCatalogs.add(mediaCatalogDTO);
         }
         return new MediaCatalogDTO(userMediaCatalog.getId(), userMediaCatalog.getName(),mediaList, subCatalogs);
--- backend/src/test/java/com/valychbreak/mymedialib/dto/catalog/MediaCatalogDTOFactoryTest.java
+++ backend/src/test/java/com/valychbreak/mymedialib/dto/catalog/MediaCatalogDTOFactoryTest.java
@@ -32,7 +32,7 @@ public class MediaCatalogDTOFactoryTest {
         UserMediaCatalog userMediaCatalog = new UserMediaCatalog("root_catalog");
         userMediaCatalog.setId(10300L);
 
-        MediaCatalogDTO mediaCatalogDTO = mediaCatalogDTOFactory.create(userMediaCatalog);
+        MediaCatalogDTO mediaCatalogDTO = mediaCatalogDTOFactory.createWithMedia(userMediaCatalog);
 
         SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(mediaCatalogDTO.getName()).isEqualTo("root_catalog");
@@ -41,10 +41,42 @@ public class MediaCatalogDTOFactoryTest {
     }
 
     @Test
+    public void createWithoutMediaWith3LevelHierarchy() throws Exception {
+        UserMediaCatalog root = new UserMediaCatalog("root");
+        UserMediaCatalog child1_1 = new UserMediaCatalog("child1_1");
+        UserMediaCatalog child1_2 = new UserMediaCatalog("child1_2");
+        UserMediaCatalog child1_2_1 = new UserMediaCatalog("child1_2_1");
+        UserMediaCatalog child1_2_2 = new UserMediaCatalog("child1_2_2");
+        UserMediaCatalog child1_2_2_1 = new UserMediaCatalog("child1_2_2_1");
+        UserMediaCatalog child1_3 = new UserMediaCatalog("child1_3");
+
+        root.getSubUserMediaCatalogs().add(child1_1);
+        root.getSubUserMediaCatalogs().add(child1_2);
+        root.getSubUserMediaCatalogs().add(child1_3);
+
+        child1_2.getSubUserMediaCatalogs().add(child1_2_1);
+        child1_2.getSubUserMediaCatalogs().add(child1_2_2);
+
+        child1_2_2.getSubUserMediaCatalogs().add(child1_2_2_1);
+
+        MediaCatalogDTO mediaCatalogDTO = mediaCatalogDTOFactory.createWithoutMedia(root);
+
+        MediaCatalogDTO expectedChild1_2_2_1 = new MediaCatalogDTO(null, "child1_2_2_1", null, new ArrayList<>());
+        MediaCatalogDTO expectedChild1_2_2 = new MediaCatalogDTO(null, "child1_2_2", null, Arrays.asList(expectedChild1_2_2_1));
+        MediaCatalogDTO expectedChild1_2_1 = new MediaCatalogDTO(null, "child1_2_1", null, new ArrayList<>());
+        MediaCatalogDTO expectedChild1_2 = new MediaCatalogDTO(null, "child1_2", null, Arrays.asList(expectedChild1_2_1, expectedChild1_2_2));
+        MediaCatalogDTO expectedChild1_1 = new MediaCatalogDTO(null, "child1_1", null, new ArrayList<>());
+        MediaCatalogDTO expectedChild1_3 = new MediaCatalogDTO(null, "child1_3", null, new ArrayList<>());
+        MediaCatalogDTO expectedRoot = new MediaCatalogDTO(null, "root", null, Arrays.asList(expectedChild1_1, expectedChild1_2, expectedChild1_3));
+
+        assertThat(mediaCatalogDTO).isEqualTo(expectedRoot);
+    }
+
+    @Test
     public void createByUserMediaCatalog() throws Exception {
         UserMediaCatalog userMediaCatalog = getUserMediaCatalogWithoutSubCatalogs();
 
-        MediaCatalogDTO mediaCatalogDTO = mediaCatalogDTOFactory.create(userMediaCatalog);
+        MediaCatalogDTO mediaCatalogDTO = mediaCatalogDTOFactory.createWithMedia(userMediaCatalog);
         assertThat(mediaCatalogDTO).isEqualTo(new MediaCatalogDTO(null, "root", Arrays.asList(new MediaFullDetailsImpl()), new ArrayList<>()));
     }
 
@@ -52,7 +84,7 @@ public class MediaCatalogDTOFactoryTest {
     public void createByUserMediaCatalogWith2LevelInSubCatalogs() throws Exception {
         UserMediaCatalog userMediaCatalog = getUserMediaCatalogWithHierarchyLevel2();
 
-        MediaCatalogDTO mediaCatalogDTO = mediaCatalogDTOFactory.create(userMediaCatalog);
+        MediaCatalogDTO mediaCatalogDTO = mediaCatalogDTOFactory.createWithMedia(userMediaCatalog);
 
         MediaCatalogDTO childLevel2 = new MediaCatalogDTO(null, "child2", Arrays.asList(new MediaFullDetailsImpl()), new ArrayList<>());
         MediaCatalogDTO childLevel1 = new MediaCatalogDTO(null, "child1", new ArrayList<>(), Arrays.asList(childLevel2));
