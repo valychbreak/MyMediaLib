5893bbe92ad1614e7d250d01d2ee79e29df0a687
Valeriy Bryliov <valychbreak@gmail.com>
2017-11-26 14:46:15 +0200

    Added population of 'known for' media to Person

--- backend/src/main/java/com/valychbreak/mymedialib/dto/movie/BasicMovieDTO.java
+++ backend/src/main/java/com/valychbreak/mymedialib/dto/movie/BasicMovieDTO.java
@@ -1,4 +1,15 @@
 package com.valychbreak.mymedialib.dto.movie;
 
 public class BasicMovieDTO {
+    private String name;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- backend/src/main/java/com/valychbreak/mymedialib/services/people/DefaultPeopleService.java
+++ backend/src/main/java/com/valychbreak/mymedialib/services/people/DefaultPeopleService.java
@@ -2,7 +2,9 @@ package com.valychbreak.mymedialib.services.people;
 
 import com.uwetrottmann.tmdb2.Tmdb;
 import com.uwetrottmann.tmdb2.entities.BasePerson;
+import com.uwetrottmann.tmdb2.entities.Media;
 import com.uwetrottmann.tmdb2.entities.PersonResultsPage;
+import com.valychbreak.mymedialib.dto.movie.BasicMovieDTO;
 import com.valychbreak.mymedialib.dto.person.BasicPersonDTO;
 import com.valychbreak.mymedialib.services.utils.SearchParams;
 import com.valychbreak.mymedialib.services.utils.SearchResult;
@@ -26,15 +28,36 @@ public class DefaultPeopleService implements PeopleService {
 
     @Override
     public SearchResult<BasicPersonDTO> search(SearchParams searchParams) throws IOException {
-        List<BasicPersonDTO> results = new ArrayList<>();
+        PersonResultsPage tmdbSearchResults = getTmdbSearchResults(searchParams);
 
-        Call<PersonResultsPage> peopleSearchResult = tmdb.searchService().person(searchParams.getQuery(), searchParams.getPage(), null, null);
-        PersonResultsPage body = peopleSearchResult.execute().body();
-        for (BasePerson basePerson : body.results) {
+        List<BasicPersonDTO> results = new ArrayList<>(tmdbSearchResults.results.size());
+        for (BasePerson basePerson : tmdbSearchResults.results) {
             BasicPersonDTO basicPersonDTO = new BasicPersonDTO(new Long(basePerson.id), basePerson.name, new ArrayList<>());
+            addKnowFor(basicPersonDTO, basePerson.known_for);
+
             results.add(basicPersonDTO);
         }
 
-        return new SearchResult<>(body.total_results, body.total_pages, results);
+        return new SearchResult<>(tmdbSearchResults.total_results, tmdbSearchResults.total_pages, results);
+    }
+
+    private void addKnowFor(BasicPersonDTO basicPersonDTO, List<Media> knownFor) {
+        for (Media media : knownFor) {
+            BasicMovieDTO basicMovieDTO = new BasicMovieDTO();
+            switch (media.media_type) {
+                case MOVIE:
+                    basicMovieDTO.setName(media.movie.title);
+                    break;
+                case TV:
+                    basicMovieDTO.setName(media.tvShow.name);
+            }
+
+            basicPersonDTO.getKnownFor().add(basicMovieDTO);
+        }
+    }
+
+    private PersonResultsPage getTmdbSearchResults(SearchParams searchParams) throws IOException {
+        Call<PersonResultsPage> peopleSearchResult = tmdb.searchService().person(searchParams.getQuery(), searchParams.getPage(), null, null);
+        return peopleSearchResult.execute().body();
     }
 }
