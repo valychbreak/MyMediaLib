664a04d98d7008a5cbfee9a22b9e3e9ea5870dbb
Valeriy Bryliov <valychbreak@gmail.com>
2018-02-25 22:56:53 +0200

    Changed OAuth client to JDBC

--- backend/src/main/java/com/valychbreak/mymedialib/OAuth2Config.java
+++ backend/src/main/java/com/valychbreak/mymedialib/OAuth2Config.java
@@ -1,11 +1,8 @@
 package com.valychbreak.mymedialib;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.AuthenticationManagerConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -13,6 +10,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
@@ -27,16 +27,14 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Value("${gigy.oauth.tokenTimeout:3600}")
 	private int expiration;
 
 	public OAuth2Config() {
-		/*userDetailsService = new UserDetailsService() {
-			@Override
-			public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-				return new User(s, "password", Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
-			}
-		};*/
+
 	}
 
 	// password encryptor
@@ -49,21 +47,18 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 	public void configure(AuthorizationServerEndpointsConfigurer configurer) throws Exception {
 		configurer.authenticationManager(authenticationManager);
 		configurer.userDetailsService(userDetailsService);
+		configurer.tokenStore(new JdbcTokenStore(dataSource));
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("gigy").secret("secret").accessTokenValiditySeconds(expiration)
-				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token").resourceIds(RESOURCE_ID);
+		clients.jdbc(dataSource);
+				/*.withClient("gigy").secret("secret").accessTokenValiditySeconds(expiration)
+				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token").resourceIds(RESOURCE_ID);*/
 	}
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.addTokenEndpointAuthenticationFilter(new SimpleCORSFilter());
-	}  /*
-
-	@Autowired
-	public void setAuthenticationManager(AuthenticationManager authenticationManager) {
-		this.authenticationManager = authenticationManager;
-	}*/
+	}
 }
