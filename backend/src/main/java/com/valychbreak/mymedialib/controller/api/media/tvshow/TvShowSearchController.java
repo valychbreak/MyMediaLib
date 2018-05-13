90e1eea002ec3692403bf414ac3d2b40904b093c
valych <valychbreak@gmail.com>
2018-05-13 03:30:19 +0300

    Fixed the issue when favorites status was not shown on MOVIE and TV SHOW search tabs. And refactored tests

--- backend/src/main/java/com/valychbreak/mymedialib/controller/api/media/tvshow/TvShowSearchController.java
+++ backend/src/main/java/com/valychbreak/mymedialib/controller/api/media/tvshow/TvShowSearchController.java
@@ -1,13 +1,13 @@
 package com.valychbreak.mymedialib.controller.api.media.tvshow;
 
-import com.omertron.omdbapi.OMDBException;
 import com.valychbreak.mymedialib.controller.api.media.MediaController;
 import com.valychbreak.mymedialib.data.movie.MediaFullDetails;
-import com.valychbreak.mymedialib.services.media.MediaSearchService;
+import com.valychbreak.mymedialib.entity.User;
+import com.valychbreak.mymedialib.services.media.UserMediaService;
 import com.valychbreak.mymedialib.services.media.tvshow.TvShowSearchService;
 import com.valychbreak.mymedialib.services.utils.SearchParamsBuilder;
 import com.valychbreak.mymedialib.services.utils.SearchResult;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.valychbreak.mymedialib.services.utils.SearchResultFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,27 +17,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class TvShowSearchController extends MediaController {
 
     private TvShowSearchService tvShowSearchService;
+    private UserMediaService userMediaService;
 
-    @Autowired
-    public TvShowSearchController(TvShowSearchService tvShowSearchService) {
+    public TvShowSearchController(TvShowSearchService tvShowSearchService, UserMediaService userMediaService) {
         this.tvShowSearchService = tvShowSearchService;
+        this.userMediaService = userMediaService;
     }
 
     @RequestMapping(value = "/tvshow/search", produces = {MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.GET)
     public ResponseEntity<SearchResult<MediaFullDetails>> movieSearch(@RequestParam(value = "q") String searchTerm,
                                                                       @RequestParam(value = "year", required = false) Integer year,
-                                                                      @RequestParam(value = "p", required = false) Integer page
-    ) throws OMDBException, IOException {
+                                                                      @RequestParam(value = "p", required = false) Integer page,
+                                                                      Principal principal
+    ) throws IOException {
 
+        User loggedUser = getUserFromPrincipal(principal);
         SearchParamsBuilder searchParamsBuilder = new SearchParamsBuilder();
         searchParamsBuilder.withQuery(searchTerm).withPage(page);
 
         SearchResult<MediaFullDetails> searchResult = tvShowSearchService.search(searchParamsBuilder.build());
-        return new ResponseEntity<>(searchResult, HttpStatus.OK);
+
+        List<MediaFullDetails> processedMediaFullDetails = searchResult.getItems().parallelStream().peek(mediaFullDetails -> {
+            boolean isFavorite = userMediaService.isUserFavorite(loggedUser, mediaFullDetails);
+            mediaFullDetails.setFavourite(isFavorite);
+        }).collect(Collectors.toList());
+
+        SearchResult<MediaFullDetails> mediaFullDetailsSearchResult = new SearchResultFactory().create(searchResult.getPage(), searchResult.getTotalPages(), searchResult.getTotalResults(), processedMediaFullDetails);
+        return new ResponseEntity<>(mediaFullDetailsSearchResult, HttpStatus.OK);
     }
 }
--- backend/src/test/java/com/valychbreak/mymedialib/controller/api/media/movie/MovieSearchControllerTest.java
+++ backend/src/test/java/com/valychbreak/mymedialib/controller/api/media/movie/MovieSearchControllerTest.java
@@ -5,11 +5,13 @@ import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.valychbreak.mymedialib.controller.ControllerTest;
 import org.junit.Test;
+import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -21,16 +23,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         TransactionalTestExecutionListener.class,
         DbUnitTestExecutionListener.class})
 @DatabaseSetup(value = "/data/db/common/CleanDb.xml", type = DatabaseOperation.DELETE_ALL)
-@DatabaseSetup(value = "/data/db/common/TestUser.xml", type = DatabaseOperation.DELETE_ALL)
+@DatabaseSetup(value = "/data/db/MediaSearchControllerTest.xml")
 public class MovieSearchControllerTest extends ControllerTest {
 
     @Test
     public void movieSearch() throws Exception {
 
-        mockMvc.perform(get("/api/movie/search?q=batman"))
+        mockMvc.perform(
+                get("/api/movie/search?q=batman")
+                        .principal(new TestingAuthenticationToken("test_user", "test12")))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.items", hasSize(20)))
                 .andExpect(jsonPath("$.items[*].type", hasItem("Movie")));
+    }
+
+    @Test
+    public void movieSearchSetsFavoriteToTrue() throws Exception {
+
+        mockMvc.perform(
+                get("/api/movie/search").param("q", "Spider-Man 3")
+                        .principal(new TestingAuthenticationToken("test_user", "test12")))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.items[*].isFavourite", contains(true, false)));
     }
 
 }
