7a0a374dcfc775177fe5205d3e40b680305eda22
valychbreak <valychbreak@gmail.com>
2019-07-19 02:27:12 +0200

    [156] Added flyway and moved oauth tables to first delta file

--- backend/src/test/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO oauth_client_details
-    (client_id, client_secret, resource_ids, scope, authorized_grant_types,
-    web_server_redirect_uri, authorities, access_token_validity,
-    refresh_token_validity, additional_information, autoapprove)
-VALUES
-    ('gigy', '$2a$10$IEfwBgnqu9XMR0G6FEUob.b7toYdiEqhe1QXfnrr7M0yKzHQjiBfC', 'resource-id', 'read,write', 'password, refresh_token', null, null, 3600, 3600, null, true);
