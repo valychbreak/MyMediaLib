919b152dfab0895828664ba9b6bdc25402adb4aa
valychbreak <valychbreak@gmail.com>
2019-08-04 01:08:41 +0200

    [161] Replaced static methods with a service and refactored test

--- backend/src/test/java/com/valychbreak/mymedialib/services/media/movie/TmdbMovieSearchServiceTest.java
+++ backend/src/test/java/com/valychbreak/mymedialib/services/media/movie/TmdbMovieSearchServiceTest.java
@@ -1,20 +1,20 @@
 package com.valychbreak.mymedialib.services.media.movie;
 
 import com.uwetrottmann.tmdb2.Tmdb;
-import com.uwetrottmann.tmdb2.entities.*;
+import com.uwetrottmann.tmdb2.entities.BaseMovie;
+import com.uwetrottmann.tmdb2.entities.MovieResultsPage;
 import com.uwetrottmann.tmdb2.services.MoviesService;
 import com.uwetrottmann.tmdb2.services.SearchService;
 import com.valychbreak.mymedialib.data.movie.MediaFullDetails;
-import com.valychbreak.mymedialib.data.movie.impl.MediaFullDetailsImpl;
 import com.valychbreak.mymedialib.services.utils.SearchParams;
 import com.valychbreak.mymedialib.services.utils.SearchParamsBuilder;
 import com.valychbreak.mymedialib.services.utils.SearchResult;
-import com.valychbreak.mymedialib.testtools.TmdbMovieBuilder;
+import com.valychbreak.mymedialib.services.utils.TmdbService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import retrofit2.Call;
 import retrofit2.Response;
 
@@ -23,12 +23,15 @@ import java.util.ArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
-// TODO: rewrite test as DefaultTvShowSearchServiceTest
 public class TmdbMovieSearchServiceTest {
+
+    private static final int CURRENT_PAGE = 3;
+    private static final int TOTAL_PAGES = 1;
+    private static final int TOTAL_RESULTS = 1;
+
 
     @Mock
     private Tmdb tmdb;
@@ -39,82 +42,43 @@ public class TmdbMovieSearchServiceTest {
     private TmdbMovieSearchService movieSearchService;
 
     @Mock
-    private MoviesService movieService;
+    private TmdbService tmdbService;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         when(tmdb.searchService()).thenReturn(searchService);
-        when(tmdb.moviesService()).thenReturn(movieService);
 
-        setupTmdbMovieService();
-        setupTmdbSearchService();
-
-        movieSearchService = new TmdbMovieSearchService(tmdb);
+        movieSearchService = new TmdbMovieSearchService(tmdb, tmdbService);
     }
 
     @Test
     public void search() throws Exception {
-        SearchParams searchParams = new SearchParamsBuilder().withPage(1).withQuery("query").build();
+        setupTmdbSearchService(TOTAL_PAGES, TOTAL_RESULTS);
+        when(tmdbService.getMovieDetails(any(BaseMovie.class))).thenReturn(mock(MediaFullDetails.class));
+
+        SearchParams searchParams = new SearchParamsBuilder()
+                .withPage(CURRENT_PAGE)
+                .withQuery("Spider-man")
+                .build();
         SearchResult<MediaFullDetails> searchResult = movieSearchService.search(searchParams);
+
+        verify(searchService).movie(eq("Spider-man"), eq(CURRENT_PAGE), eq(null), eq(null), eq(null), eq(null), eq(null));
         assertThat(searchResult).isNotNull();
-        assertThat(searchResult.getTotalPages()).isEqualTo(1);
-        assertThat(searchResult.getTotalResults()).isEqualTo(1);
-
-        MediaFullDetailsImpl mediaFullDetails = getExpectedMediaFullDetails();
-        assertThat(searchResult.getItems()).hasSize(1);
-        assertThat(searchResult.getItems().iterator().next()).isEqualToComparingFieldByField(mediaFullDetails);
+        assertThat(searchResult.getTotalPages()).isEqualTo(TOTAL_PAGES);
+        assertThat(searchResult.getTotalResults()).isEqualTo(TOTAL_RESULTS);
     }
 
-    private MediaFullDetailsImpl getExpectedMediaFullDetails() {
-        MediaFullDetailsImpl mediaFullDetails = new MediaFullDetailsImpl();
-        mediaFullDetails.setTitle("Fight club");
-        mediaFullDetails.setGenre("Adventure");
-        mediaFullDetails.setDuration("116");
-        mediaFullDetails.setImagePath("");
-        mediaFullDetails.setStars("9.66");
-        mediaFullDetails.setReviews("1337");
-        mediaFullDetails.setType("Movie");
-        mediaFullDetails.setImdbId("tt123");
-        return mediaFullDetails;
-    }
-
-    private void setupTmdbSearchService() throws java.io.IOException {
-        BaseMovie baseMovie = new BaseMovie();
-        baseMovie.id = 1;
-        baseMovie.title = "Fight club";
-        baseMovie.media_type = "Movie";
-
-        ArrayList<BaseMovie> baseMovies = new ArrayList<>();
-        baseMovies.add(baseMovie);
-
+    private void setupTmdbSearchService(int totalPages, int totalResults) throws java.io.IOException {
         MovieResultsPage movieResultsPage = new MovieResultsPage();
-        movieResultsPage.total_pages = 1;
-        movieResultsPage.total_results = 1;
-        movieResultsPage.results = baseMovies;
+        movieResultsPage.total_pages = totalPages;
+        movieResultsPage.total_results = totalResults;
+        movieResultsPage.results = new ArrayList<>();
         Response<MovieResultsPage> response = Response.success(movieResultsPage);
 
         Call<MovieResultsPage> mockedCall = mock(Call.class);
         when(mockedCall.execute()).thenReturn(response);
         when(searchService.movie(any(String.class), any(Integer.class), eq(null), eq(null), eq(null), eq(null), eq(null)))
                 .thenReturn(mockedCall);
-    }
-
-    private void setupTmdbMovieService() throws java.io.IOException {
-        Movie fullMovie = new TmdbMovieBuilder()
-                .setId(1)
-                .setTitle("Fight club")
-                .setMediaType("Movie")
-                .setBackdropPath("")
-                .setImdbId("tt123")
-                .setVoteAverage(9.66D)
-                .setVoteCount(1337)
-                .build();
-
-        Response<Movie> movieResponse = Response.success(fullMovie);
-
-        Call<Movie> mockedMovieCall = mock(Call.class);
-        when(mockedMovieCall.execute()).thenReturn(movieResponse);
-        when(movieService.summary(any(Integer.class), eq(null), any(AppendToResponse.class))).thenReturn(mockedMovieCall);
     }
 
 }
