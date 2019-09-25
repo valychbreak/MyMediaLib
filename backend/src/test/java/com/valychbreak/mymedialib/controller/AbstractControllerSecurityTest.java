ca0414c4880e583ca304f8c694b3e55ea3545dcc
valychbreak <valychbreak@gmail.com>
2019-09-25 21:29:15 +0200

    [S158] Removed dependency on spring boot runner and moved json method to static class

--- backend/src/test/java/com/valychbreak/mymedialib/controller/AbstractControllerSecurityTest.java
+++ backend/src/test/java/com/valychbreak/mymedialib/controller/AbstractControllerSecurityTest.java
@@ -1,15 +1,15 @@
 package com.valychbreak.mymedialib.controller;
 
-import com.google.gson.Gson;
 import com.valychbreak.mymedialib.Application;
 import com.valychbreak.mymedialib.testtools.OAuth2AccessTokenProvider;
-import com.valychbreak.mymedialib.utils.gson.GsonBuilderTools;
 import org.junit.Before;
-import org.junit.runner.RunWith;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
@@ -20,18 +20,24 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @WebAppConfiguration
 @TestPropertySource(locations = "classpath:application.yml")
 public abstract class AbstractControllerSecurityTest {
 
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
+
     @Autowired
     protected WebApplicationContext webapp;
 
-    protected OAuth2AccessTokenProvider accessTokenProvider;
-
     protected MockMvc mockMvc;
+
+    private OAuth2AccessTokenProvider accessTokenProvider;
 
     @Before
     public void before() {
@@ -41,11 +47,6 @@ public abstract class AbstractControllerSecurityTest {
                 .build();
 
         accessTokenProvider = new OAuth2AccessTokenProvider(mockMvc);
-    }
-
-    protected String json(Object object) {
-        Gson gson = GsonBuilderTools.getGsonBuilder().create();
-        return gson.toJson(object);
     }
 
     protected RequestPostProcessor bearerToken(String username, String password) {
