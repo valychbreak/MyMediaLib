cac9f005908d3410c7815e7431a0509e32574fcd
valychbreak <valychbreak@gmail.com>
2019-09-28 10:23:23 +0200

    [S152] Added url params when doing a search

--- frontend/src/main/frontend/src/app/views/movies/movies.component.ts
+++ frontend/src/main/frontend/src/app/views/movies/movies.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AbstractForm} from "../../base/form";
 import {SearchParams} from "../../shared/search/search-params";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
     selector: 'app-movie',
@@ -10,20 +11,50 @@ import {SearchParams} from "../../shared/search/search-params";
 export class MoviesComponent extends AbstractForm implements OnInit {
     searchParams: SearchParams;
 
-    constructor() {
+    constructor(private router: Router, private activatedRoute: ActivatedRoute) {
         super()
     }
 
     ngOnInit() {
-        this.searchParams = new SearchParams();
-        this.searchParams.page = 1;
+        let queryParam = this.activatedRoute.snapshot.queryParamMap.get('q');
+        let pageParam = this.activatedRoute.snapshot.queryParamMap.get('p');
 
+        this.searchParams = this.createSearchParams(queryParam, pageParam);
     }
 
     submitSearch(searchParams: SearchParams, isValid: boolean): void {
         this.searchParams = new SearchParams();
         this.searchParams.query = searchParams.query;
         this.searchParams.page = 1;
+
+        this.changeUrlQueryParams(this.searchParams);
+    }
+
+
+    private changeUrlQueryParams(searchParams: SearchParams) {
+        let queryParams = {
+            q: searchParams.query,
+            p: String(searchParams.page)
+        };
+
+        this.router.navigate([], {relativeTo: this.activatedRoute, queryParams, queryParamsHandling: "merge"})
+    }
+
+    private createSearchParams(searchQuery: string, pageParam: string): SearchParams {
+        let searchParams = new SearchParams();
+
+        if (searchQuery) {
+            searchParams.query = searchQuery;
+        }
+
+        let page = parseInt(pageParam);
+        if (searchQuery && !isNaN(page)) {
+            searchParams.page = page;
+        } else {
+            searchParams.page = 1;
+        }
+
+        return searchParams;
     }
 
 }
